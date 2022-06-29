49ff063baf7ab0c14172e67edf5db91e0f40ea26
Paul Ashby <paul@primitive.co>
2022-06-29 11:16:00 +0100

    Add utility function for database calls

--- models/RateLimiter.php
+++ models/RateLimiter.php
@@ -14,38 +14,29 @@ Class RateLimiter {
 	/**
 	 * Check that request is within rate limit
 	 *
-	 * @param ipv6   IP address
+	 * @param ipv6: IP address (ipv4 or ipv6)
 	 * @return Boolean
 	 */ 
-	public function limit($ipv6) {
+	public function limit($ip_address) {
 
 		// Prune request table so we're not counting requests outside the current time window
 		$window_time = time() % 60; // Number of seconds remaining in current time window
-		$query = "DELETE FROM requests WHERE req_time <= DATE_SUB(NOW(), INTERVAL $window_time SECOND) AND ipv6 = INET6_ATON(:ipv6)";
-		$stmt = $this->conn->prepare($query);
-		$stmt->bindParam('ipv6', $ipv6, PDO::PARAM_STR);
-		$stmt->execute();
+		$this->db_query("DELETE FROM requests WHERE req_time <= DATE_SUB(NOW(), INTERVAL $window_time SECOND) AND ipv6 = INET6_ATON(:ipv6)", $ip_address);
 		$time_now = time();
 		$window_time_remaining = 60 - ($time_now % 60);
 
 		// Get number of requests from this ip address
-		$query = "SELECT COUNT(ipv6) FROM requests WHERE ipv6 = INET6_ATON(:ipv6)";
-		$stmt = $this->conn->prepare($query);
-		$stmt->bindParam('ipv6', $ipv6, PDO::PARAM_STR);
-		$stmt->execute();
+		$stmt = $this->db_query("SELECT COUNT(ipv6) FROM requests WHERE ipv6 = INET6_ATON(:ipv6)", $ip_address);
 		$request_count = $stmt->fetchColumn();
 
 		$permitted = $request_count < $this->limit;
 
 		if($permitted) {
 			// Add a record for this request;
-			$query = "INSERT INTO requests (ipv6, req_time) VALUES (INET6_ATON(:ipv6), NOW()) ON DUPLICATE KEY UPDATE req_time=req_time";
-			$stmt = $this->conn->prepare($query);
-			$stmt->bindParam('ipv6', $ipv6, PDO::PARAM_STR);
-			$stmt->execute();
+			$this->db_query("INSERT INTO requests (ipv6, req_time) VALUES (INET6_ATON(:ipv6), NOW()) ON DUPLICATE KEY UPDATE req_time=req_time", $ip_address);
 			$request_count++;
 		}
-		error_log($request_count);
+		
 		return array(
 			'X_RateLimit_Reset' 	=> $time_now + $window_time_remaining,
 			'X_RateLimit_Remaining'	=> $this->limit - $request_count,
@@ -53,5 +44,20 @@ Class RateLimiter {
 			'Retry_After' 			=> $window_time_remaining,
 			'permitted'				=> $permitted
 		);
+	}
+
+	/**
+	 * Make call to database
+	 *
+	 * @param query: Query string
+	 * @param ip_address: ipv4 or ipv6
+	 * @return Response
+	 */ 
+	private function db_query($query, $ip_address) {
+		$stmt = $this->conn->prepare($query);
+		$stmt->bindParam('ipv6', $ip_address, PDO::PARAM_STR);
+		$stmt->execute();
+
+		return $stmt;
 	}
 }
