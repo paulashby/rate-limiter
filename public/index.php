2b4d307659cad63aa70e6af65f187dd9b729d473
Paul Ashby <paul@primitive.co>
2022-07-04 19:14:41 +0100

    Expect headers from SlidingWindow rate limiter Limiter now accepts arg for mitigating suspected DDOS attacks (throttle requests for everyone to protect the server)

--- public/index.php
+++ public/index.php
@@ -6,35 +6,35 @@ header("Content-Type: application/json");
 // Use output buffering so we can update headers
 ob_start();
 
-include_once "../models/RateLimiter.php";
+include_once "../models/SlidingWindow.php";
 
-define("REQUESTS_PER_MINUTE", 2);
+define("REQUESTS_PER_MINUTE", 100);
 $ip = $_SERVER['REMOTE_ADDR'];
-$limiter = new SlidingWindow(REQUESTS_PER_MINUTE);
-$limit_status = $limiter->limit($ip);
 
-header('X-RateLimit-Limit: ' . REQUESTS_PER_MINUTE);
-header('X-RateLimit-Remaining: ' . $limit_status['X_RateLimit_Remaining']);
-header('X-RateLimit-Reset: ' . $limit_status['X_RateLimit_Reset']);
+$limiter = new SlidingWindow(REQUESTS_PER_MINUTE, true);
+$response_data = $limiter->limit($ip);
 
-if($limit_status['permitted']){
-	$out = array(
-		"Success" => True,
-		"Data" => "Some juicy stuff",
-		"Window" => time()
-	);
-	echo json_encode($out);	
-} else {
-	header('HTTP/1.1 429 Too Many Requests', true, 429);
-	header('Retry-After: ' . $limit_status['Retry_After']);
-
-	$out = array(
-		"Success" => False,
-		"Code" => "429 - Too Many Requests",
-		"Window" => time() % 60
-	);
-	die("Too many requests");
+foreach ($response_data['headers'] as $header) {
+	if(is_array($header)){
+		// Splat array into separate arguments for function call
+		header(... $header);
+	} else {
+		// Single argument provided
+		header($header);
+	}
 }
+
+if(!$response_data['permitted']) {
+	die($response_data['message']);
+}
+
+echo json_encode(
+	array(
+		"Success" => True,
+		"Data" => "Some juicy stuff"
+	)
+);
+
 $output = ob_get_contents();
 ob_end_clean();
 
